1c0fad927b7e9c916fd447a8d39f960a63926140
nichmor <nmorkotilo@gmail.com>
2022-07-27 20:50:47 +0300

    Add github actions with codecov workflow

--- tests/test_repo_observer.py
+++ tests/test_repo_observer.py
@@ -1,2 +1,6 @@
+from ci import repo_observer
+
 def test_all_good():
+    arg_string = ["--dispatcher-server", "localhost:8888" , "test_repo"]
+    repo_observer.parse_args(arg_string)
     assert True
