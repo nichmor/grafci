d9a97713dcb23c621bdc92aa8a98f4735016c6ac
nichmor <nmorkotilo@gmail.com>
2022-07-28 20:29:47 +0300

    Add full test coverage for repo_observer

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,28 +1,12 @@
 import socket
-import pytest
+import time
 from threading import Thread
+from unittest.mock import MagicMock, patch
 from ci import helpers
 
-PORT = 9999
 
-
-def run_test_socket_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', PORT))
-    server_socket.listen(1)
-    print('listening')
-    conn, addr = server_socket.accept()
-    print('accepted')
-    data = conn.recv(1024)
-    conn.sendall(data)
-    conn.close()
-
-
-def test_communicate():
-    server_thread = Thread(target=run_test_socket_server, daemon=True)
-    print('before run')
-    server_thread.start()
-    print('before communicate')
-    response = helpers.communicate('localhost', PORT, 'test_data')
-    assert  response == 'test_data'
-    server_thread.join()
+@patch('ci.helpers.socket.socket')
+def test_communicate(socket_mock: MagicMock):
+    socket_mock().recv.return_value = 'test_data'.encode('utf-8')
+    response = helpers.communicate('127.0.0.1', 9999, 'test_data')
+    assert response == 'test_data'
--- tests/test_repo_observer.py
+++ tests/test_repo_observer.py
@@ -1,8 +1,9 @@
+import argparse
+import socket
 import subprocess
-import unittest
 from ci import repo_observer
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, mock_open, patch
 
 
 
@@ -11,7 +12,6 @@ def test_parse_args():
     namespace = repo_observer.parse_args(arg_string)
     assert namespace.dispatcher_server == "localhost:8888"
     assert namespace.repo == "test_repo"
-
 
 @patch("ci.repo_observer.subprocess.check_output")
 def test_update_repository(mock_check_output):
@@ -25,3 +25,52 @@ def test_update_repository_raise_exception(mock_check_output):
         repo_observer.update_repository("test_repo")
         assert "test_cmd" in system_error
 
+@patch("ci.repo_observer.helpers.communicate")
+def test_verify_dispatcher_status(communicate_mock):
+    communicate_mock.return_value = "OK"
+    assert "OK" == repo_observer.verify_dispatcher_status("localhost", 9999)
+
+@patch("ci.repo_observer.helpers.communicate", side_effect=socket.error("system_error"))
+def test_verify_dispatcher_status_raise_exception(communicate_mock):
+    with pytest.raises(ConnectionError) as connection_error:
+        repo_observer.verify_dispatcher_status("localhost", 9999)
+        assert "system_error" in connection_error
+
+@patch("ci.repo_observer.open", new_callable=mock_open, read_data='commit_1')
+@patch("ci.repo_observer.helpers.communicate")
+def test_dispatch_commit_for_test(mock_communicate, mock_open):
+    mock_communicate.return_value = 'OK'
+    repo_observer.dispatch_commit_for_test('localhost', 9999)
+    mock_communicate.assert_called_with('localhost', 9999, 'dispatch:commit_1')
+
+@patch("ci.repo_observer.open", new_callable=mock_open, read_data='commit_1')
+@patch("ci.repo_observer.helpers.communicate")
+def test_dispatch_commit_for_test_raise_exception(mock_communicate, mock_open):
+    with pytest.raises(ConnectionError):
+        mock_communicate.return_value = 'NOT_OK'
+        repo_observer.dispatch_commit_for_test('localhost', 9999)
+
+@patch("ci.repo_observer.os.path.isfile")
+@patch("ci.repo_observer.dispatch_commit_for_test")
+@patch("ci.repo_observer.verify_dispatcher_status")
+@patch("ci.repo_observer.update_repository")
+def test_poll(update_repository_mock, verify_dispatcher_status, dispatch_mock, is_file_mock):
+    is_file_mock.return_value = True
+    verify_dispatcher_status.return_value = 'OK'
+    repo_observer._poll("localhost", "9999", "test_repo")
+
+@patch("ci.repo_observer.os.path.isfile")
+@patch("ci.repo_observer.update_repository")
+def test_poll_no_commit(update_repository_mock, is_file_mock):
+    is_file_mock.return_value = False
+    repo_observer._poll("localhost", "9999", "test_repo")
+
+
+@patch("ci.repo_observer.os.path.isfile")
+@patch("ci.repo_observer.verify_dispatcher_status")
+@patch("ci.repo_observer.update_repository")
+def test_poll_raise_exception(update_repository_mock, verify_dispatcher_status,  is_file_mock):
+    is_file_mock.return_value = True
+    verify_dispatcher_status.return_value = 'NOT_OK'
+    with pytest.raises(ConnectionError):
+        repo_observer._poll("localhost", "9999", "test_repo")
